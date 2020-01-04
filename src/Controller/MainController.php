934b28eca128186feea0b7d4bce0bdb212128d79
StickHash <chrisb1981@orange.fr>
2020-01-04 10:15:41 +0100

    improving main controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,7 @@ use App\Entity\Category;
 use App\Repository\CategoryRepository;
 use App\Repository\KnowledgeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
@@ -13,6 +14,17 @@ use Symfony\Component\Serializer\Serializer;
 
 class MainController extends AbstractController
 {
+    private $encoders = [];
+    private $normalizers = [];
+    private $serializer;
+
+    public function __construct()
+    {
+        $this->encoders = [new JsonEncoder()];
+        $this->normalizers = [new ObjectNormalizer()];
+        $this->serializer = new Serializer($this->normalizers, $this->encoders);
+    }
+
     /**
      * @Route("/main", name="main")
      */
@@ -26,48 +38,39 @@ class MainController extends AbstractController
 
     /**
      * @Route("/categories", name="categories")
+     * @param CategoryRepository $categoryRepository
+     * @return JsonResponse
      */
     public function showCategories(CategoryRepository $categoryRepository)
     {
-        $encoders = [new JsonEncoder()];
-        $normalizers = [new ObjectNormalizer()];
-
-        $serializer = new Serializer($normalizers, $encoders);
 
         $categories = $categoryRepository->findAll();
 
-        return $this->json($serializer->serialize($categories, 'json', ['attributes' => ['id', 'name']]));
+        return $this->json($this->serializer->serialize($categories, 'json', ['attributes' => ['id', 'name']]));
     }
 
     /**
      * @Route("/all", name="all")
+     * @param KnowledgeRepository $knowledgeRepository
+     * @return JsonResponse
      */
     public function all(KnowledgeRepository $knowledgeRepository)
     {
-        $encoders = [new JsonEncoder()];
-        $normalizers = [new ObjectNormalizer()];
-
-
-        $serializer = new Serializer($normalizers, $encoders);
-
         $knowledges = $knowledgeRepository->findAll();
 
-        return $this->json($serializer->serialize($knowledges, 'json', ['attributes' => ['name', 'imgUrl', 'category' => ['name']]]));
+        return $this->json($this->serializer->serialize($knowledges, 'json', ['attributes' => ['name', 'imgUrl', 'category' => ['name']]]));
     }
 
     /**
      * @Route("/knowledge/{id}", name="knowledge")
+     * @param Category $category
+     * @return JsonResponse
      */
     public function knowledgeByCategory(Category $category)
     {
-        $encoders = [new JsonEncoder()];
-        $normalizers = [new ObjectNormalizer()];
-
-
-        $serializer = new Serializer($normalizers, $encoders);
 
         $knowledges = $category->getKnowledges();
 
-        return $this->json($serializer->serialize($knowledges, 'json', ['attributes' => ['name', 'category' => ['name']]]));
+        return $this->json($this->serializer->serialize($knowledges, 'json', ['attributes' => ['name', 'category' => ['name']]]));
     }
 }
