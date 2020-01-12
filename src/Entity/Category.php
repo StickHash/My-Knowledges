545795932c13d248f08dfc1d40d85fb93a96312d
StickHash <chrisb1981@orange.fr>
2020-01-12 10:00:37 +0100

    adding API Endpoints

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,10 +2,15 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ * @ApiResource(
+ *     collectionOperations={"get"},
+ *     itemOperations={"get"}
+ * )
  */
 class Category
 {
